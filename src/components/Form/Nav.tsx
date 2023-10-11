64340c5b289d74162068f8d76f0f5c37d9e18d9c
DemonGod <mr.dndung@gmail.com>
2023-10-11 15:48:58 +0700

    v2.1

--- src/components/Form/Nav.tsx
+++ src/components/Form/Nav.tsx
@@ -21,7 +21,7 @@ const Nav = (props: { children?: ReactNode}) => {
     <Header as="header">
     <Heading color={colors.primary} size="large">
       <img width="64" src="/web-check.png" alt="Web Check Icon" />
-      <a href="/">Web Check</a>
+      <a href="/">Kiểm tra trang web</a>
     </Heading>
       {props.children && props.children}
   </Header>
